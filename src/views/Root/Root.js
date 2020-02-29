ca2dbbb607a34cef54f0c9ba5490ded381e71578
dominik <jurasz.do@gmail.com>
2020-02-29 18:04:18 +0100

    Added storybook

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import GlobalStyles from 'themes/GlobalStyles';
+import { theme } from 'themes/MainTheme';
+import { ThemeProvider } from 'styled-components';
+
+import Button from 'components/atoms/Button/Button';
 
 const Root = () => (
   <div>
-    <GlobalStyles/>
-    <h1>Hello World!</h1>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Hello World!</h1>
+        <Button>Hello</Button>
+      </>
+    </ThemeProvider>
   </div>
 );
 
