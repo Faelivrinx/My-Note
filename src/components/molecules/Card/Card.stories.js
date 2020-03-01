2f06a914d88ce021cd355bccd48d9af46dc15025
dominik <jurasz.do@gmail.com>
2020-03-01 17:29:46 +0100

    Added card component (resolves #19)

---
+++ src/components/molecules/Card/Card.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './Card';
+
+export default {
+  component: 'Card',
+  title: 'Card',
+};
+
+export const primary = () => <Card />;
+export const secondary = () => <Card cardType="twitter" />;
+export const tertiary = () => <Card cardType="article" />;
